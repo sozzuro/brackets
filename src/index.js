949e2207e1c2396738e5f7fd1a5f7cf00feeeb85
Denis Radivil <sozzuro@gmail.com>
2020-10-20 14:40:03 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while (true) {
+        let flagCheckEnd = false;
+        bracketsConfig.forEach(item => {
+            if (str.includes(item[0] + item[1])) {
+                str = str.replace(item[0] + item[1], '')
+                flagCheckEnd = true;
+            }
+        })
+        if (!flagCheckEnd) {
+            break;
+        }
+    }
+    return (str === '') ? true : false;
 }
